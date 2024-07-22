945dd91bc80ec56d455bad29b6f1f8683d530bbc
deanmastria <mastriadata@gmail.com>
2024-07-22 14:54:34 -0400

    Parts 2 and 3

---
+++ src/HiFriend.java
@@ -0,0 +1,7 @@
+public class HiFriend {
+    public static void main(String[] args) {
+        String friendFirst = "Larry";
+        String friendSecond = "Sprinkle";
+        System.out.println("Hi " + friendFirst + " " + friendSecond + "!");
+    }
+}
